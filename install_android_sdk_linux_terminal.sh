a4d31d7dc412406b34ec02599f4f81eb1110fad8
Márcio Almeida <mdma@cin.ufpe.br>
2020-07-30 17:53:30 -0300

    Script editado e primeiros testes iniciados.

--- install_android_sdk_linux_terminal.sh
+++ install_android_sdk_linux_terminal.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
+#https://stackoverflow.com/questions/60730615/sdkmanager-warning-could-not-create-setting-java-lang-illegalargumentexcepti
 
-https://stackoverflow.com/questions/60730615/sdkmanager-warning-could-not-create-setting-java-lang-illegalargumentexcepti
-
-https://stackoverflow.com/questions/60440509/android-command-line-tools-sdkmanager-always-shows-warning-could-not-create-se
-
-Access all Android licenses
-yes Y | /opt/android/tools/bin/sdkmanager --licenses
+#https://stackoverflow.com/questions/60440509/android-command-line-tools-sdkmanager-always-shows-warning-could-not-create-se
 
 clear
 echo ""
@@ -16,49 +11,33 @@ echo "=== Instalação de Android SDK ==="
 echo ""
 echo ""
 
-echo "1. Baixar SDK para a home do usuário"
+echo "1. Criar diretório /opt/android-sdk"
+mkdir /opt/android-sdk
 
-echo "2. Extrair arquivo android-sdk-linux-terminal.zip na Home do usuário"
-unzip $HOME/android-sdk-linux-terminal.zip
+echo "2. Baixar SDK para o diretório /opt/android-sdk"
+wget -O /opt/android-sdk/android-sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
 
-echo "3. Dar as permissões corretas à pasta $HOME/android-sdk-linux-terminal"
-sudo chmod -R +x $HOME/android-sdk-linux-terminal
+echo "3. Extrair arquivo android-sdk.zip no diretório /opt/android-sdk"
+unzip /opt/android-sdk/android-sdk.zip
 
-echo "4. Entrar na pasta $HOME/android-sdk-windows-terminal\tools\bin"
-cd $HOME/android-sdk-windows-terminal/tools/bin
+echo "4. Dar as permissões corretas à pasta /opt/android-sdk"
+chmod -R +x /opt/android-sdk
 
-echo "5. Fazer download e configurar Platform Tools, Tools e Build Tools"
-echo "OBS: será necessário aceitar as licenças manualmente."
+echo "5. Entrar na pasta /opt/android-sdk/tools/bin"
+cd /opt/android-sdk/tools/bin
+
+echo "6. Fazer download e configurar Platform Tools, Tools e Build Tools"
+yes Y | /opt/android-sdk/tools/bin/sdkmanager --licenses
 ./sdkmanager tools platform-tools $(./sdkmanager --list | grep build-tools | tail -n1 | cut -d" " -f3)
 
-echo "6. Adicionar links simbólicos do Platform Tools, Tools e Build Tools à pasta /usr/bin"
+echo "7. Adicionar links simbólicos do Platform Tools, Tools e Build Tools à pasta /usr/bin"
 cd /usr/bin
-sudo ln -fs $HOME/android-sdk-linux-terminal/platform-tools/adb .
-sudo ln -fs $HOME/android-sdk-linux-terminal/platform-tools/fastboot .
-sudo ln -fs $HOME/android-sdk-linux-terminal/build-tools/$(ls -1 $HOME/android-sdk-linux-terminal/build-tools/ | tail -n1)/aapt .
+ln -fs /opt/android-sdk/platform-tools/adb .
+ln -fs /opt/android-sdk/platform-tools/fastboot .
+ln -fs /opt/android-sdk/build-tools/$(ls -1 /opt/android-sdk/build-tools/ | tail -n1)/aapt .
 
-echo "7. Mover o arquivo $HOME/51-android.rules para o diretório /etc/udev/rules.d/"
-https://snowdream.github.io/51-android/
-sudo mv $HOME/51-android.rules /etc/udev/rules.d/
+echo "8. Baixar o arquivo 51-android.rules para o diretório /etc/udev/rules.d/"
+wget -o /etc/udev/rules.d/51-android.rules https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
 
-echo "8. Dar permissão de leitura ao arquivo 51-android.rules em /etc/udev/rules.d/"
-sudo chmod a+r /etc/udev/rules.d/51-android.rules
-
-echo "11. Desabilitar timestamp"
-sudo su
-echo 0 > /proc/sys/net/ipv4/tcp_timestamps
-sysctl -w net.ipv4.tcp_timestamps=0
-
-cd $HOME
-echo "12. Testar ADB"
-adb devices
-
-sleep 2
-
-echo "13. Testar Fastboot"
-fastboot devices
-
-sleep 2
-
-echo "14. Testar AAPT"
-aapt
+echo "9. Dar permissão de leitura ao arquivo 51-android.rules em /etc/udev/rules.d/"
+chmod a+r /etc/udev/rules.d/51-android.rules
