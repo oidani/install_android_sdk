35d1065bacd48d73724029f952844458f50db3f3
Márcio Almeida <mdma@cin.ufpe.br>
2020-08-05 18:16:56 -0300

    Ajustes de código

--- docker/update-android-sdk_linux.sh
+++ docker/update-android-sdk_linux.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-echo -e "Downloading and configuring newest versions of the following packages:"
+echo -e "1. Downloading and configuring newest versions of the following packages:"
 echo -e "- Tools\n- Platform-Tools\n- Build-Tools"
+
 cd /opt/android-sdk/cmdline-tools/tools/bin/
-yes | ./sdkmanager tools platform-tools $(./sdkmanager --list | grep build-tools | tail -n1 | cut -d" " -f3) 2> /dev/null
+yes | sdkmanager tools platform-tools $(sdkmanager --list | grep build-tools | tail -n1 | cut -d" " -f3) 2>&1 /dev/null
 
 if [ $? == "0" ]; then
-    echo -e "\nUpdating simbolic links in /usr/bin"
+    echo -e "\n2. Updating simbolic links in /usr/bin"
+    echo -e "\n2.1. adb"
     ln -fs /opt/android-sdk/platform-tools/adb /usr/bin/
+    echo -e "\n2.2. fastboot"
     ln -fs /opt/android-sdk/platform-tools/fastboot /usr/bin/
+    echo -e "\n2.3. aapt"
     ln -fs /opt/android-sdk/build-tools/$(ls -1 /opt/android-sdk/build-tools/ | tail -n1)/aapt /usr/bin/
-    echo -e "\n Update process completed."
-else; then
-    echo -e "Update process failed. Please try again later."
+    echo -e "\nSTATUS: update process completed."
+else
+    echo -e "\nSTATUS: update process failed. Please try again later."
     exit 1
 fi
-
 exit 0
--- linux/install-android-sdk_linux.sh
+++ linux/install-android-sdk_linux.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#https://stackoverflow.com/questions/60730615/sdkmanager-warning-could-not-create-setting-java-lang-illegalargumentexcepti
-
-#https://stackoverflow.com/questions/60440509/android-command-line-tools-sdkmanager-always-shows-warning-could-not-create-se
-
 clear
 echo ""
 echo ""
@@ -11,33 +7,37 @@ echo "=== Instalação de Android SDK ==="
 echo ""
 echo ""
 
-echo "1. Criar diretório /opt/android-sdk"
-mkdir /opt/android-sdk
+echo "1. Criar diretório /opt/android-sdk/cmdline-tools/"
+mkdir /opt/android-sdk/cmdline-tools/
 
-echo "2. Baixar SDK para o diretório /opt/android-sdk"
-wget -O /opt/android-sdk/android-sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
+echo "2. Baixar SDK para o diretório /opt/android-sdk/cmdline-tools/"
+wget -O /opt/android-sdk/cmdline-tools/android-sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
 
-echo "3. Extrair arquivo android-sdk.zip no diretório /opt/android-sdk"
-unzip /opt/android-sdk/android-sdk.zip
+echo "3. Extrair arquivo android-sdk.zip no diretório /opt/android-sdk/cmdline-tools/"
+unzip /opt/android-sdk/cmdline-tools/android-sdk.zip
 
-echo "4. Dar as permissões corretas à pasta /opt/android-sdk"
-chmod -R +x /opt/android-sdk
+echo "4. Remover arquivo android-sdk.zip"
+rm /opt/android-sdk/cmdline-tools/android-sdk.zip
+
+echo "5. Dar permissão de execução à pasta /opt/android-sdk/"
+chmod -R +x /opt/android-sdk/
 
 echo "5. Entrar na pasta /opt/android-sdk/tools/bin"
-cd /opt/android-sdk/tools/bin
+cd /opt/android-sdk/cmdline-tools/tools/bin/
 
-echo "6. Fazer download e configurar Platform Tools, Tools e Build Tools"
-yes Y | /opt/android-sdk/tools/bin/sdkmanager --licenses
-./sdkmanager tools platform-tools $(./sdkmanager --list | grep build-tools | tail -n1 | cut -d" " -f3)
+echo "6. Criar link simbólico para o sdkmanager em /usr/bin/"
+ln -fs /opt/android-sdk/cmdline-tools/tools/bin/sdkmanager /usr/bin/
 
-echo "7. Adicionar links simbólicos do Platform Tools, Tools e Build Tools à pasta /usr/bin"
-cd /usr/bin
-ln -fs /opt/android-sdk/platform-tools/adb .
-ln -fs /opt/android-sdk/platform-tools/fastboot .
-ln -fs /opt/android-sdk/build-tools/$(ls -1 /opt/android-sdk/build-tools/ | tail -n1)/aapt .
+echo "7. Fazer download e configurar Platform Tools, Tools e Build Tools"
+yes | sdkmanager tools platform-tools $(sdkmanager --list | grep build-tools | tail -n1 | cut -d" " -f3)
 
-echo "8. Baixar o arquivo 51-android.rules para o diretório /etc/udev/rules.d/"
-wget -o /etc/udev/rules.d/51-android.rules https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
+echo "8. Adicionar links simbólicos para Platform Tools, Tools e Build Tools à pasta /usr/bin/"
+ln -fs /opt/android-sdk/platform-tools/adb /usr/bin/
+ln -fs /opt/android-sdk/platform-tools/fastboot /usr/bin/
+ln -fs /opt/android-sdk/build-tools/$(ls -1 /opt/android-sdk/build-tools/ | tail -n1)/aapt /usr/bin/
 
-echo "9. Dar permissão de leitura ao arquivo 51-android.rules em /etc/udev/rules.d/"
+echo "9. Baixar o arquivo 51-android.rules para o diretório /etc/udev/rules.d/"
+wget -O /etc/udev/rules.d/51-android.rules https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
+
+echo "10. Dar permissão de leitura ao arquivo 51-android.rules em /etc/udev/rules.d/"
 chmod a+r /etc/udev/rules.d/51-android.rules
